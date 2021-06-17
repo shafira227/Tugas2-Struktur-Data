0106525faff6ae0ed9d6433ca5f6fc646bbaf964
shafira227 <58319233+shafira227@users.noreply.github.com>
2021-06-17 15:02:36 +0800

    Add files via upload

---
+++ R2.2.py
@@ -0,0 +1,5 @@
+#Soal : Give an example of a software application in which adaptability can mean
+#the difference between a prolonged lifetime of sales and bankruptcy.
+
+#Beradaptasi berarti kode tersebut fleksibel sehingga dapat dijalankan di berbagai jenis perangkat keras dan berbeda
+#sistem operasi. Sebuah perusahaan harus mampu merespon dengan cepat ketika lingkungan berubah.
