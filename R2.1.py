e21c1282dbf1d494fcecbe027300d62580df50d0
shafira227 <58319233+shafira227@users.noreply.github.com>
2021-06-17 14:43:52 +0800

    Add files via upload

---
+++ R2.1.py
@@ -0,0 +1,6 @@
+#Soal 1 : Give three examples of life-critical software applications.
+
+#Adapun 3 contoh aplikasi life-critical software yaitu : 
+# 1. Sistem Operasi (Windows, Linux, MAC OS)
+# 2. Microsoft Office
+# 3. Google Chrome
