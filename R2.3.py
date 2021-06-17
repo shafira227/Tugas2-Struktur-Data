b00af56a569cee9120996a92de143d5a2f835f87
shafira227 <58319233+shafira227@users.noreply.github.com>
2021-06-17 14:47:00 +0800

    Add files via upload

---
+++ R2.3.py
@@ -0,0 +1,11 @@
+#Soal 3: Describe a component from a text-editor GUI
+#and the methods that it encapsulates
+
+
+
+# Dokumen akan memiliki atribut _content yang akan berisi semua karakter dalam dokumen.
+# Metode:
+#   1. copy(): ini akan menyalin urutan karakter tertentu dan menyimpannya di beberapa atribut _clipboard
+#   2. paste(): ini akan memasukkan konten _clipboard ke posisi dan rentang yang ditentukan di _content
+#   3. insert(): menyisipkan karakter ke posisi tertentu dalam urutan
+#   4. delete(): menghapus karakter dari _content
